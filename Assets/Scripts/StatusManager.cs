21bc97ceffdbdc626002baf72a0fb63235364798
shahan-kimura <suteado01092@gmail.com>
2024-10-20 15:02:02 +0900

    ■StatusManagerの追加、Enemyの撃破、作成したPrefabのPackage化
    □作成したPrefabのPackage化
    ・過去のUnityProjectを開き、Package化したいPrefabを右クリック>ExportPackage>名前を付けて保存
    ・読み込み先のProjectからAssets>InportPackage>CustomPackage
    
    □StatusManagerの追加
    ・StatusManager.csの追加、個別にHP等のステータス管理が可能に
    ・MainObjectを指定（今まで通りアタッチされたオブジェクトを直接削除するDestroy（GameObject)でもいいが、個別にMainObjectを指定する形式だと部位破壊みたいな事をしたい際に便利）
    ・事前にInportPackageで読み込んでおいたParticleSystemのEffectをDamage、Destoryに指定

---
+++ Assets/Scripts/StatusManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusManager : MonoBehaviour
+{
+    [SerializeField] GameObject MainObject; //このスクリプトをアタッチするオブジェクト
+    [SerializeField] int hp = 1;             //hp現在値
+    [SerializeField] int maxHp = 1;          //いずれmaxHp利用する際に使用
+
+    [SerializeField] GameObject destroyEffect;  //撃破エフェクト
+    [SerializeField] GameObject damageEffect;   //被弾エフェクト
+
+    [SerializeField] string TagName;            //当たり判定となるタグ
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hpが0以下なら、撃破エフェクトを生成してMainを破壊
+        if (hp <= 0)
+        {
+            DestoryMainObject();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+        //当たり判定のタグネームと一致していればダメージ処理呼び出し
+        if (other.CompareTag(TagName))
+        {
+            Damage();
+        }
+    }
+    private void Damage()
+    {
+        hp--;
+        var effect = Instantiate(damageEffect);
+        effect.transform.position = transform.position;
+    }
+    private void DestoryMainObject()
+    {
+        hp = 0;
+        var effect = Instantiate(destroyEffect);
+        effect.transform.position = transform.position;
+        Destroy(effect, 5);
+        Destroy(MainObject);
+    }
+
+}
