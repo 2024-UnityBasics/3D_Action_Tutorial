45ae28e7eebf4a54906ef16cae39c896788cf070
shahan kimura(304) <sha-kimura@athuman.com>
2024-11-01 14:08:15 +0900

    ■EnemyManagerの作成（２）、EnemyGanaratorの作成（１）
    ■EnemyManagerの作成（２）
    ・前回補足：Managerは、シーン内の空オブジェクトにアタッチしよう！
    ・前回のままだと、無限にEnemy Clearのログが出てしまう
    ・bool isExplorationPhaseでフラグを管理し、敵数が0かつフラグが立っていない時だけ敵数を確認
    ・フラグが再度切り替わる条件のためにWaitUntil関数を利用
    
    EnemｙGanaratorの作成（１）
    ・Player接触式のGanarator
    ・OnTrigger形式でEnemyを生成する
    ・InstantiateするPrefabを指定し、pop数はfor文で制御、生成間隔はWaitForSecondで変えられるように
    ・出現場所はEnemyGanaratorを中心にしつつ、Random.Rangeでオフセット範囲を持たせられるように
    ・ただし現状だと、接触するたびにEnemyが生成されてしまう

--- Assets/Scripts/Manager/EnemyManager.cs
+++ Assets/Scripts/Manager/EnemyManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     private int currentEnemyCount = 0; // 現在の敵数
+    private bool isExplorationPhase = false; // 探索フェーズの制御フラグ
 
     private void Start()
     {
@@ -18,13 +19,24 @@ public class EnemyManager : MonoBehaviour
         {
             yield return new WaitForSeconds(1f); // 1秒ごとに確認（必要に応じて調整）
 
+            // デバッグ用ログ
+            Debug.Log("Current Enemy Count: " + currentEnemyCount); // 現在の敵数をログに出力
+
             // Enemyタグを持つオブジェクトの数を取得(.Lengthで数が取得できる)
             currentEnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-
-            // 敵がすべて倒された場合
-            if (currentEnemyCount <= 0)
+            if (currentEnemyCount <= 0 && !isExplorationPhase)
             {
-                Debug.Log("Enemy Clear");
+                isExplorationPhase = true;  // 探索フェーズに切り替え
+
+                Debug.Log("Enemy Clear");   //敵がすべて倒された判定、演出やフェーズ変更等を入れてもよい
+
+            // 敵が再度出現するまで待機
+            while (currentEnemyCount <= 0)
+            {
+                currentEnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length; // 敵の数を再確認
+                yield return new WaitForSeconds(1f); // 1秒ごとに再確認
+            }
+                isExplorationPhase = false; // 再度敵が出現したら探索フェーズを終了
             }
         }
     }
