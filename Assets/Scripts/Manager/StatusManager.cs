6fd76ef7ddc720c8a9a6d7944f18330c611ee401
shahan-kimura <suteado01092@gmail.com>
2024-11-17 22:21:58 +0900

    ■ダメージのばらつきと、基本的なクリティカル判定の実装
    ・StatusManager.csのDamageメソッド内に処理を追加

--- Assets/Scripts/Manager/StatusManager.cs
+++ Assets/Scripts/Manager/StatusManager.cs
@@ -13,6 +13,9 @@ public class StatusManager : MonoBehaviour
     [SerializeField] GameObject destroyEffect;  //撃破エフェクト
     [SerializeField] GameObject damageEffect;   //被弾エフェクト
 
+    [SerializeField] float criticalRate = 0.3f; // クリティカル発生率（0～1）
+    [SerializeField] float criticalMultiplier = 2f; // クリティカル発生時のダメージ倍率
+
     // Update is called once per frame
     void Update()
     {
@@ -24,17 +27,31 @@ public class StatusManager : MonoBehaviour
     }
 
     // 被ダメージ時の処理
-    public void Damage(int damage)
+    public void Damage(int baseDamage)
     {
-        Debug.Log($"{name} は {damage} ダメージを受けます");
+        // ランダム補正を計算（-20%～+20%の範囲）
+        float randomFactor = Random.Range(0.8f, 1.2f);
+        float finalDamage = baseDamage * randomFactor;
 
-        hp -= damage;                               // 引数damageの分だけHPを減少
+        // クリティカル判定
+        if (Random.value < criticalRate) // Random.value は 0～1の範囲でランダムな値を返す
+        {
+            finalDamage *= criticalMultiplier; // クリティカル発生でダメージを増加
+            Debug.Log($"{name} はクリティカルヒット！ ダメージ: {finalDamage}");
+        }
+        else
+        {
+            Debug.Log($"{name} に通常ダメージ: {finalDamage}");
+        }
+
+        // HPを減少
+        hp -= Mathf.RoundToInt(finalDamage);
 
         var effect = Instantiate(damageEffect);     // ダメージエフェクトの生成
         effect.transform.position = transform.position; // ダメージエフェクトの生成場所の指定
 
         DamagePopupManager manager = FindObjectOfType<DamagePopupManager>(); // Managerを検索
-        manager.ShowDamage(damage, transform.position); // ダメージポップアップ表示
+        manager.ShowDamage(Mathf.RoundToInt(finalDamage), transform.position); // ダメージポップアップ表示
     }
 
     // アタッチされたオブジェクトが破壊される際の処理
